4340cd27f6dc8abfdd5d3a9423f07b2237a3074c
Tu Tran <tu.tran@temple.edu>
2017-11-06 04:05:02 -0500

    add master code

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -24,11 +24,14 @@ int main(int argc, char* argv[])
   double *cc2;	/* A x B computed using the conventional algorithm */
 
   int a_row, a_col, b_col;
-
+  int num_sent, sender;
   int myid, numprocs;
   double starttime, endtime;
   MPI_Status status;
+
   /* insert other global variables here */
+  double *buffer;
+
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -47,7 +50,7 @@ int main(int argc, char* argv[])
     printf("Matrix A:\n");
     for (int i = 0; i < a_row; i++) {
       for (int j = 0; j < a_col; j++) {
-        printf("%.1f ", aa[i * a_row + j]);
+        printf("%.1f ", aa[i * a_col + j]);
       }
       printf("\n");
     }
@@ -55,23 +58,65 @@ int main(int argc, char* argv[])
     printf("Matrix B:\n");
     for (int i = 0; i < a_col; i++) {
       for (int j = 0; j < b_col; j++) {
-        printf("%.1f ", bb[i * a_col + j]);
+        printf("%.1f ", bb[i * b_col + j]);
       }
       printf("\n");
     }
 
-    /**
+    nrows = a_row;
+    ncols = b_col;
+    buffer = (double *) malloc(sizeof(double) * a_col);
+
     if (myid == 0) {
       // Master Code goes here
-      aa = gen_matrix(nrows, ncols);
-      bb = gen_matrix(ncols, nrows);
-      cc1 = malloc(sizeof(double) * nrows * nrows); 
+      cc1 = malloc(sizeof(double) * nrows * ncols);
       starttime = MPI_Wtime();
 
+      // number of rows sent
+      num_sent = 0;
       // Insert your master code here to store the product into cc1
+      // broadcast the matrix bb to all slaves
+      MPI_Bcast(bb, a_col * b_col, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+      // sending a row of aa to slaves at a time
+      for (int i = 0; i < min(numprocs - 1, nrows), i++) {
+        for (int j = 0; j < a_col; j++) {
+          buffer[j] = aa[i * a_col + j];
+        }
+        MPI_Send(buffer, a_col, MPI_DOUBLE, i + 1, i + 1, MPI_COMM_WORLD);
+        num_sent++;
+      }
+
+      // data to get back from slaves
+      double *received = (double *) malloc(sizeof(double) * ncols);
+
+      for (int i = 0; i < nrows; i++) {
+        MPI_Recv(received, ncols, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
+            MPI_COMM_WORLD, &status);
+        sender = status.MPI_SOURCE;
+        anstype = status.MPI_TAG;  // row number from slaves
+
+        // fill cc1 with data from slaves
+        for (int j = 0; j < ncols; j++) {
+          cc1[(anstype - 1) * ncols + j] = received[j];
+        }
+
+        // if there are still data to send
+        if (num_sent < nrows) {
+          for (int j = 0; j < a_col; j++) {
+            buffer[j] = aa[num_sent * a_col + j];
+          }
+
+          MPI_Send(buffer, a_col, MPI_DOUBLE, sender, num_sent + 1, MPI_COMM_WORLD);
+          num_sent++;
+        } else {
+          // send a STOP signal to the slaves
+          MPI_Send(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
+        }
+      }
+
       endtime = MPI_Wtime();
       printf("%f\n",(endtime - starttime));
-      cc2  = malloc(sizeof(double) * nrows * nrows);
+      cc2  = malloc(sizeof(double) * nrows * ncols);
       mmult(cc2, aa, nrows, ncols, bb, ncols, nrows);
       compare_matrices(cc2, cc1, nrows, nrows);
     } else {
@@ -81,7 +126,5 @@ int main(int argc, char* argv[])
     fprintf(stderr, "Usage matrix_times_vector <size>\n");
   }
   MPI_Finalize();
-  */
-  }
   return 0;
 }
